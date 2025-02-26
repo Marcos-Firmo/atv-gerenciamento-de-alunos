257e755ac6330683d5d4328366088b1d6807b114
Marcos Firmino <145605055+Marcos-Firmo@users.noreply.github.com>
2025-02-26 01:23:16 -0300

    Primeira Questão
    foi colocado apenas um "const" para fazer o registro dos dados de três alunos.

---
+++ gerenciamentoAlunos.js
@@ -0,0 +1,26 @@
+/*1. Definição de Registro: Crie um registro chamado aluno com os seguintes campos:
+nome: Nome do aluno(a) (string)
+idade: Idade do aluno(a) (número inteiro)
+matricula: Matrícula do aluno(a) (número inteiro)
+curso: Código do curso do aluno(a) (string), por exemplo, EC, SI, CC, etc.*/
+
+const aluno = [
+    {
+        nome: "Marcos",
+        idade: 20,
+        matricula: 563224,
+        curso: "CC"
+    },
+    { 
+        nome: "Luiza", 
+        idade: 17, 
+        matricula: 862379, 
+        curso: "SI" 
+    },
+    { 
+        nome: "Pedro", 
+        idade: 19, 
+        matricula: 368572, 
+        curso: "EC" 
+    }
+]
